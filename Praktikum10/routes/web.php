566d056f0ea2e4d1c6a2c81d6fbd27024de00b4c
najlaalthfip <najlaalthfp@gmail.com>
2024-05-29 22:44:08 +0700

    Praktikum11

--- Praktikum10/routes/web.php
+++ Praktikum10/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PasienController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,8 @@ Route::get('/admin/index', function () {
 Route::get('/admin/pegawai', function () {
     return view('admin.pegawai');
 });
+
+// pasien
+Route::get('/admin/pasien', [PasienController::class, 'index']);
 
 ?>
